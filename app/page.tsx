35b1bb8a00e69135f9ea63f1b4297f9e87214c34
Shaaf <shaaf.m.shahzad@gmail.com>
2023-12-21 20:44:27 -0500

    Added prettier config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-<<<<<<< HEAD
-  return <main>Hello World</main>;
-=======
-	return <main>Hello Linus!</main>;
->>>>>>> refs/remotes/origin/master
+	return <main>Hello World</main>;
 }
