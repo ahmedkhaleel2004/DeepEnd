ed8afee6a4d8b9aceb7dbb7f48dc0723b645d144
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-21 22:26:31 -0500

    fix firebase auth

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,16 @@ const signInWithGithub = () => {
 			console.log("working");
 			// This gives you a GitHub Access Token. You can use it to access the GitHub API.
 			const credential = GithubAuthProvider.credentialFromResult(result);
-			const token = credential.accessToken;
+			const token = credential?.accessToken;
 
 			// The signed-in user info.
 			const user = result.user;
 			// ...
 		})
 		.catch((error) => {
-			console.log("not working");
+			console.log(`not working`);
 			// Handle Errors here.
 			const errorCode = error.code;
-			console.log(process.env.API_KEY);
 			const errorMessage = error.message;
 			// The email of the user's account used.
 			const email = error.customData.email;
@@ -34,6 +33,7 @@ export default function Home() {
 	return (
 		<main>
 			Hello benji!
+			<br />
 			<button onClick={signInWithGithub}>Sign in with GitHub</button>
 		</main>
 	);
