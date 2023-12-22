56d9e2db071969dc28de253b38323747ad383f91
Shaaf <shaaf.m.shahzad@gmail.com>
2023-12-21 23:18:55 -0500

    Updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -32,10 +32,20 @@ const signInWithGithub = () => {
 export default function Home() {
 	return (
 		<div className="flex flex-col h-screen justify-center items-center">
-			<p className="text-3xl font-extrabold italic">Welcome to Linus</p>
-			<button onClick={signInWithGithub} className="">
-				Sign in with GitHub
-			</button>
+			<div
+				id="container"
+				className="w-1/4 h-1/4 border-zinc-800 border-4 rounded-xl flex flex-col justify-center items-center gap-10"
+			>
+				<p className="text-3xl font-extrabold italic text-center">
+					Welcome to Linus
+				</p>
+				<button
+					onClick={signInWithGithub}
+					className="w-1/2 h-1/4 border-zinc-800 border-2 rounded-xl hover:bg-zinc-800 hover:text-white transition duration-500 ease-in-out"
+				>
+					Sign in with GitHub
+				</button>
+			</div>
 		</div>
 	);
 }
