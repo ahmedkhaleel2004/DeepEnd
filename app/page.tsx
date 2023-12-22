21c4742af7e8bda7604f686bc94c4a65dacf0bb9
Benjamin Avdullahu <000876877@mohawkcollege.ca>
2023-12-21 23:45:45 -0500

    signing in/out update

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,31 @@
 "use client";
 
 import React from "react";
-import { signInWithPopup, GithubAuthProvider } from "firebase/auth";
+import { signInWithPopup, GithubAuthProvider, signOut } from "firebase/auth";
 import { auth, provider } from "./firebase";
 
-const signInWithGithub = () => {
-	signInWithPopup(auth, provider)
-		.then((result) => {
-			console.log("working");
-			// This gives you a GitHub Access Token. You can use it to access the GitHub API.
-			const credential = GithubAuthProvider.credentialFromResult(result);
-			const token = credential?.accessToken;
+const signInWithGithub = async () => {
+	try {
+		const result = await signInWithPopup(auth, provider);
+		console.log("working");
 
-			// The signed-in user info.
-			const user = result.user;
-			// ...
+		// This gives you a GitHub Access Token. You can use it to access the GitHub API.
+		const credential = GithubAuthProvider.credentialFromResult(result);
+		const token = credential?.accessToken;
+
+		// user info
+		const user = result.user;
+	} catch (err) {
+		console.error(err);
+	}
+};
+const logout = async () => {
+	signOut(auth)
+		.then(() => {
+			// Sign-out successful.
 		})
 		.catch((error) => {
-			console.log(`not working`);
-			// Handle Errors here.
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			// The email of the user's account used.
-			const email = error.customData.email;
-			// The AuthCredential type that was used.
-			const credential = GithubAuthProvider.credentialFromError(error);
-			// ...
+			// An error happened.
 		});
 };
 
