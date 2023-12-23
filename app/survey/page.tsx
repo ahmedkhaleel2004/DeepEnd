f691a8a458f8119d689e83962d46b43095997f44
Shaaf <shaaf.m.shahzad@gmail.com>
2023-12-23 03:01:09 -0500

    Updated routes at survey page

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -7,24 +7,37 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
 import { signOutFunc } from "@/lib/firebase";
-import { auth } from "@/lib/firebase";
+import { auth, db } from "@/lib/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 const Survey = () => {
 	const router = useRouter();
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			if (user) {
-				setLoading(false);
-			} else {
-				router.push("/");
-			}
-		});
+		const checkAuthState = async () => {
+			const unsubscribe = onAuthStateChanged(auth, async (user) => {
+				if (user) {
+					const docRef = doc(db, "users", user.uid);
+					const docSnap = await getDoc(docRef);
+					if (docSnap.data()?.doneSurvey) {
+						router.push("/projects");
+					} else if (!docSnap.data()?.doneSurvey) {
+						setLoading(false);
+					} else {
+						console.log("error");
+					}
+				} else {
+					router.push("/");
+				}
+			});
 
-		// Cleanup subscription on unmount
-		return () => unsubscribe();
+			// Cleanup subscription on unmount
+			return () => unsubscribe();
+		};
+
+		checkAuthState();
 	}, [router]);
 
 	const handleSignOut = async () => {
