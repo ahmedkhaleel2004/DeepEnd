1e0cbac7cee460fdab49d5bca9e286cec0774f08
Shaaf <shaaf.m.shahzad@gmail.com>
2023-12-23 02:43:05 -0500

    Fixed projects routing

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,24 +3,37 @@
 import React, { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { auth } from "@/lib/firebase";
+import { auth, db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const Projects = () => {
 	const router = useRouter();
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-			if (user) {
-				setLoading(false);
-			} else {
-				router.push("/");
-			}
-		});
+		const checkAuthState = async () => {
+			const unsubscribe = onAuthStateChanged(auth, async (user) => {
+				if (user) {
+					const docRef = doc(db, "users", user.uid);
+					const docSnap = await getDoc(docRef);
+					if (docSnap.data()?.doneSurvey) {
+						setLoading(false);
+					} else if (!docSnap.data()?.doneSurvey) {
+						router.push("/survey");
+					} else {
+						console.log("error");
+					}
+				} else {
+					router.push("/");
+				}
+			});
 
-		// Cleanup subscription on unmount
-		return () => unsubscribe();
-	}, [router]);
+			// Cleanup subscription on unmount
+			return () => unsubscribe();
+		};
+
+		checkAuthState();
+	}, []);
 
 	if (loading) {
 		return <div>Loading...</div>; // Or replace with a loading spinner
