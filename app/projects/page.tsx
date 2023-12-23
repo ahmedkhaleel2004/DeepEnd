bfd194c44dc51bd096ab57fa1664e636abfe897c
Shaaf <shaaf.m.shahzad@gmail.com>
2023-12-23 02:29:25 -0500

    Updated projects page with client side rendering

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,30 @@
-// import { redirect } from "next/navigation";
-// import { auth } from "@/lib/firebase";
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
+import { auth } from "@/lib/firebase";
 
 const Projects = () => {
-	// if (!auth.currentUser) {
-	// 	redirect("/");
-	// }
+	const router = useRouter();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setLoading(false);
+			} else {
+				router.push("/");
+			}
+		});
+
+		// Cleanup subscription on unmount
+		return () => unsubscribe();
+	}, [router]);
+
+	if (loading) {
+		return <div>Loading...</div>; // Or replace with a loading spinner
+	}
 
 	return (
 		<div>
