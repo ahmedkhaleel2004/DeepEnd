fc70a271d25b9bbbe3c5855ec2c350faf85b62e2
Benjamin Avdullahu <000876877@mohawkcollege.ca>
2023-12-23 02:50:49 -0500

    signing in/out update

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -43,14 +43,11 @@ const Survey = () => {
 		<>
 			<div className="flex justify-between items-center pr-4 pt-4">
 				<div>Logo</div>
-				<ProfileIcon />
+				<ProfileIcon onSignOut={handleSignOut} />
 			</div>
 			<Card className="p-4 flex w-[40vh]">
 				<InputForm />
 			</Card>
-			<div className="p-4">
-				<Button onClick={handleSignOut}>Sign Out</Button>
-			</div>
 		</>
 	);
 };
