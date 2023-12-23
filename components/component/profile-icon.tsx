42389492a83931a68cdf06ca22d02f2e6f525e60
Benjamin Avdullahu <000876877@mohawkcollege.ca>
2023-12-23 02:50:49 -0500

    Sign out Profile

--- components/component/profile-icon.tsx
+++ components/component/profile-icon.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { signOutFunc } from "@/lib/firebase";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { doc, getDoc } from "firebase/firestore";
-import { auth, db } from "@/lib/firebase";
+import {
+	DropdownMenuTrigger,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuContent,
+	DropdownMenu,
+} from "@/components/ui/dropdown-menu";
 
-const ProfileIcon = () => {
+const ProfileIcon = ({ onSignOut }: { onSignOut: () => void }) => {
 	return (
-		<Avatar className="align-right">
-			<AvatarImage src="" />
-			<AvatarFallback>CN</AvatarFallback>
-		</Avatar>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Avatar className="align-right h-9 w-9">
+					<AvatarImage src="https://github.com/shadcn.png" />
+					<AvatarFallback>CN</AvatarFallback>
+				</Avatar>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem onClick={onSignOut}>
+					Sign Out
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 };
 
