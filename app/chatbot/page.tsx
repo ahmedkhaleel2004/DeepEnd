56a9f4c1126e9ececd0ee3aa1d40525761d34f0a
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-23 21:19:28 -0500

    Create chatbot page

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { auth, db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+const Chatbot = () => {
+	const router = useRouter();
+
+	useEffect(() => {
+		const checkAuthState = async () => {
+			const unsubscribe = onAuthStateChanged(auth, async (user) => {
+				if (user) {
+					const docRef = doc(db, "users", user.uid);
+					const docSnap = await getDoc(docRef);
+					if (!docSnap.data()?.doneSurvey) {
+						router.push("/survey");
+					}
+				} else {
+					router.push("/");
+				}
+			});
+
+			// Cleanup subscription on unmount
+			return () => unsubscribe();
+		};
+
+		checkAuthState();
+	}, [router]);
+
+	return <div>Chatbot</div>;
+};
+
+export default Chatbot;
