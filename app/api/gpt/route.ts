30b4ac38dcb1496bb7eb625f3d30a50b2ce120c7
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-24 01:22:50 -0500

    implement openai api endpoint

---
+++ app/api/gpt/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+// Make sure to replace 'your-api-key' with your actual OpenAI API key
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+async function complete() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "What is 1+1?" }],
+    model: "gpt-4",
+  });
+
+  return completion.choices[0];
+}
+
+export async function GET() {
+    const completion = await complete();
+    return NextResponse.json(completion);
+}
