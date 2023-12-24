a8fb42e4e1fb5baa024018745fb48034a49709e3
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-24 04:29:59 -0500

    add user pfp to chatbot

--- components/component/message.tsx
+++ components/component/message.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { useState, useEffect } from "react";
+import { auth } from "@/lib/firebase";
 
 interface MessageProps {
 	key: number;
@@ -10,17 +12,36 @@ interface MessageProps {
 }
 
 const Message: React.FC<MessageProps> = ({ role, content }) => {
+	const [photoURL, setPhotoURL] = useState<string | null>("");
+
+	useEffect(() => {
+		if (role !== "Linus") {
+			const unsubscribe = auth.onAuthStateChanged((user) => {
+				if (user && user.photoURL) {
+					setPhotoURL(user.photoURL);
+				}
+			});
+
+			// Cleanup subscription on unmount
+			return () => unsubscribe();
+		}
+	}, []);
+
+	const avatarSrc =
+		role === "Linus" ? "https://github.com/shadcn.png" : photoURL;
+
 	return (
 		<div className="flex items-start space-x-4 mb-4">
 			<Avatar>
-				<AvatarImage
-					src="https://github.com/shadcn.png"
-					alt="@shadcn"
-				/>
-				<AvatarFallback>CN</AvatarFallback>
+				{avatarSrc ? (
+					<AvatarImage src={avatarSrc} />
+				) : (
+					<AvatarFallback>CN</AvatarFallback>
+				)}
 			</Avatar>
+
 			<div>
-				<Badge className="mb-1">{role}</Badge>
+				<Badge className="mb-2">{role}</Badge>
 				<Card>
 					<CardContent className="px-2 py-1">
 						<p className="p-2">{content}</p>
