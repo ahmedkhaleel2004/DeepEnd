7e99a18054c2a5b54af34ebe918fd8194650818f
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-24 23:28:13 -0500

    Improve survey components

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -1,18 +1,59 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { InputForm } from "@/components/component/input-form";
-import ProfileIcon from "@/components/component/profile-icon";
-import { Card } from "@/components/ui/card";
+import React, { useEffect, useState } from "react";
+import { FormQuestion } from "@/components/component/input-form";
 import { useRouter } from "next/navigation";
-import { signOutFunc } from "@/lib/firebase";
 import { auth, db } from "@/lib/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import Navbar from "@/components/component/navbar";
+import { Card } from "@/components/ui/card";
+
+// Define your questions
+const questions = [
+	{
+		title: "Question 1",
+		placeholder: "Placeholder 1",
+		desc: "Description 1",
+	},
+	{
+		title: "Question 2",
+		placeholder: "Placeholder 2",
+		desc: "Description 2",
+	},
+	{
+		title: "Question 3",
+		placeholder: "Placeholder 3",
+		desc: "Description 3",
+	},
+	{
+		title: "Question 4",
+		placeholder: "Placeholder 4",
+		desc: "Description 4",
+	},
+	{
+		title: "Question 5",
+		placeholder: "Placeholder 5",
+		desc: "Description 5",
+	},
+	// Add more questions as needed
+];
 
 const Survey = () => {
 	const router = useRouter();
+	// Add a state variable for the current question index
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+	// Get the current question
+	const currentQuestion = questions[currentQuestionIndex];
+	// Function to go to the next question
+	const nextQuestion = () => {
+		if (currentQuestionIndex < questions.length - 1) {
+			setCurrentQuestionIndex(currentQuestionIndex + 1);
+		}
+	};
+	const handleSuccessfulSubmit = () => {
+		nextQuestion();
+	};
 
 	useEffect(() => {
 		const checkAuthState = async () => {
@@ -37,12 +78,21 @@ const Survey = () => {
 
 	return (
 		<>
-			<>
+			<main className="max-w-2xl mx-auto">
 				<Navbar mainPage={false} />
-				<Card className="p-4 flex w-[40vh]">
-					<InputForm />
+				<div className="mb-4">
+					<h1 className="text-3xl font-bold">Survey</h1>
+					<p>Please answer the following questions</p>
+				</div>
+				<Card className="p-4">
+					<FormQuestion
+						title={currentQuestion.title}
+						placeholder={currentQuestion.placeholder}
+						desc={currentQuestion.desc}
+						onSuccessfulSubmit={handleSuccessfulSubmit}
+					/>
 				</Card>
-			</>
+			</main>
 		</>
 	);
 };
--- components/component/input-form.tsx
+++ components/component/input-form.tsx
@@ -16,43 +16,55 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-const FormSchema = z.object({
-	username: z.string().min(2, {
-		message: "Username must be at least 2 characters.",
+const formSchema = z.object({
+	input: z.string().min(2, {
+		message: "Input must be at least 2 characters.",
 	}),
 });
 
-export function InputForm() {
-	const form = useForm<z.infer<typeof FormSchema>>({
-		resolver: zodResolver(FormSchema),
+interface FormQuestionProps {
+	title: string;
+	placeholder: string;
+	desc: string;
+	onSuccessfulSubmit: () => void;
+}
+
+export function FormQuestion({
+	title,
+	placeholder,
+	desc,
+	onSuccessfulSubmit,
+}: FormQuestionProps) {
+	// 1. Define your form.
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
 		defaultValues: {
-			username: "",
+			input: "",
 		},
 	});
 
-	function onSubmit(data: z.infer<typeof FormSchema>) {
-		console.log(data);
+	// 2. Define a submit handler.
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		// This will be type-safe and validated.
+		console.log(values);
+		form.reset({ input: "" });
+		onSuccessfulSubmit();
 	}
 
 	return (
 		<Form {...form}>
-			<form
-				onSubmit={form.handleSubmit(onSubmit)}
-				className="w-2/3 space-y-6"
-			>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
 				<FormField
 					control={form.control}
-					name="username"
+					name="input"
 					render={({ field }) => (
 						<FormItem>
-							<FormLabel>prop.input-title</FormLabel>
+							<FormLabel>{title}</FormLabel>
 							<FormControl>
-								<Input
-									placeholder="prop.placeholder"
-									{...field}
-								/>
+								<Input placeholder={placeholder} {...field} />
 							</FormControl>
-							<FormDescription>prop.desc</FormDescription>
+							<FormDescription>{desc}</FormDescription>
 							<FormMessage />
 						</FormItem>
 					)}
