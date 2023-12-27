a7613622c2ad1d2db85f4a13fb3e66ed2f20ca1a
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-26 20:12:02 -0500

    desktop codeblock width fix

--- components/component/message.tsx
+++ components/component/message.tsx
@@ -45,7 +45,7 @@ const ChatMessage: React.FC<MessageProps> = ({ role, content }) => {
 			<div className="mt-2">
 				<strong>{role}</strong>
 				<MemoizedReactMarkdown
-					className="max-w-xs sm:max-w-xl md:max-w-2xl mr-[2rem] lg:max-w-full prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
+					className="max-w-xs sm:max-w-xl md:max-w-2xl mr-[2rem] lg:max-w-none prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
 					remarkPlugins={[remarkGfm, remarkMath]}
 					components={{
 						p({ children }) {
