79f9e2b655fa0b93dd5e41ba7b5e1458c7c78cdc
Shaaf <shaaf.m.shahzad@gmail.com>
2023-12-26 22:15:36 -0500

    Added navbar functionality

--- components/component/navbars/navbar-small.tsx
+++ components/component/navbars/navbar-small.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import React from "react";
+import {
+	NavigationMenu,
+	NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
 
 const NavbarSmall = () => {
-	return <div>NavbarSmall</div>;
+	const navbarItems = ["Projects", "Chatbot", "Timeline"];
+
+	return (
+		<NavigationMenu>
+			<div>
+				{navbarItems.map((item, index) => (
+					<NavigationMenuLink key={index}>
+						<a
+							href={`/${item.toLowerCase()}`}
+							className="px-2 py-1"
+						>
+							{item}
+						</a>
+					</NavigationMenuLink>
+				))}
+			</div>
+		</NavigationMenu>
+	);
 };
 
 export default NavbarSmall;
