c21966222611a6edce40c40114393324668d5a83
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-26 22:50:00 -0500

    set max_tokens

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -15,6 +15,7 @@ export async function POST(req: Request) {
 	// Ask OpenAI for a streaming chat completion given the prompt
 	const response = await openai.chat.completions.create({
 		model: "gpt-4-1106-preview",
+		max_tokens: 1000,
 		stream: true,
 		messages,
 	});
