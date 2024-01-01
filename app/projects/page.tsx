35ce7300f32b6c2e233e0df889578e5c46ea2075
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-01 00:44:13 -0500

    fix projects loading

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,6 +11,8 @@ import ListItem from "@/components/component/projects/list-item";
 import { getLanguages } from "@/lib/get-repos";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Button } from "@/components/ui/button";
+import { motion, AnimatePresence } from "framer-motion";
+import { IconPlus } from "@/components/ui/icons";
 
 const recommended_projects = [
 	{
@@ -79,94 +81,112 @@ const Projects = () => {
 			<div className="border flex flex-row w-full max-w-full">
 				<NavbarLarge projects={true} />
 			</div>
-			<main className="flex p-12">
-				<div className="flex-col flex">
+			<main className="flex p-12 justify-between">
+				<div className="flex-col flex w-full">
 					<div className="flex justify-between items-center">
 						<div>
 							<h1 className="text-3xl font-bold">My Projects</h1>
 							<p>Click on any project to edit information</p>
 						</div>
-						<Button className="mr-8">Add Project</Button>
+						<Button className="mr-8">
+							<IconPlus className="mr-2" />
+							Add Project
+						</Button>
 					</div>
-					<div className="max-w-full min-w-full">
-						{userData && (
-							<ScrollArea className="h-[70vh]">
-								<GridContainer>
-									{userData?.repositories
-										.sort((a: any, b: any) => {
-											const aPoints =
-												JSON.parse(a.points)
-													?.bullet_points || [];
-											const bPoints =
-												JSON.parse(b.points)
-													?.bullet_points || [];
-											if (
-												aPoints.length > 0 &&
-												bPoints.length === 0
-											) {
-												return -1; // if a has points and b doesn't, a comes first
-											}
-											if (
-												aPoints.length === 0 &&
-												bPoints.length > 0
-											) {
-												return 1; // inversely
-											}
-											return 0; // if both have points or both don't, order doesn't matter
-										})
-										.map(
-											(
-												repository: any,
-												index: number
-											) => (
-												<GridItem
-													key={index}
-													title={repository.name}
-													description={
-														repository.description ||
-														""
-													}
-													points={
-														JSON.parse(
-															repository.points
-														)?.bullet_points || [
-															"",
-															"",
-															"",
-														]
-													}
-													languages={
-														languages[
-															repository.name
-														] || {}
-													}
-												/>
-											)
-										)}
-								</GridContainer>
-							</ScrollArea>
-						)}
+					<div className="w-full">
+						<AnimatePresence>
+							{userData ? (
+								<ScrollArea className="h-[70vh]">
+									<GridContainer>
+										{userData?.repositories
+											.sort((a: any, b: any) => {
+												const aPoints =
+													JSON.parse(a.points)
+														?.bullet_points || [];
+												const bPoints =
+													JSON.parse(b.points)
+														?.bullet_points || [];
+												if (
+													aPoints.length > 0 &&
+													bPoints.length === 0
+												) {
+													return -1; // if a has points and b doesn't, a comes first
+												}
+												if (
+													aPoints.length === 0 &&
+													bPoints.length > 0
+												) {
+													return 1; // inversely
+												}
+												return 0; // if both have points or both don't, order doesn't matter
+											})
+											.map(
+												(
+													repository: any,
+													index: number
+												) => (
+													<GridItem
+														key={index}
+														title={repository.name}
+														description={
+															repository.description ||
+															""
+														}
+														points={
+															JSON.parse(
+																repository.points
+															)
+																?.bullet_points || [
+																"",
+																"",
+																"",
+															]
+														}
+														languages={
+															languages[
+																repository.name
+															] || {}
+														}
+													/>
+												)
+											)}
+									</GridContainer>
+								</ScrollArea>
+							) : (
+								<motion.div
+									initial={{ opacity: 1 }}
+									animate={{ opacity: 0 }}
+									exit={{ opacity: 0 }}
+									className="text-xl text-muted-foreground flex justify-center items-center w-full h-[70vh]"
+								>
+									{" "}
+									Loading...{" "}
+								</motion.div>
+							)}
+						</AnimatePresence>
 					</div>
 				</div>
-				<Separator
-					orientation="vertical"
-					className="h-auto mx-12 bg-white dark:bg-zinc-700"
-				/>
-				<div className="flex-col flex min-w-max flex-grow max-w-xl">
-					<div className="mb-6">
-						<h1 className="text-3xl font-bold">
-							Recommended Projects
-						</h1>
-						<p>Choose a project to view</p>
-					</div>
-					<div className="space-y-8">
-						{recommended_projects.map((project, index) => (
-							<ListItem
-								key={index}
-								title={project.title}
-								description={project.description}
-							/>
-						))}
+				<div className="flex">
+					<Separator
+						orientation="vertical"
+						className="h-auto mx-12 bg-white dark:bg-zinc-700"
+					/>
+					<div className="flex-col flex min-w-max flex-grow max-w-xl">
+						<div className="mb-6">
+							<h1 className="text-3xl font-bold">
+								Recommended Projects
+							</h1>
+							<p>Choose a project to view</p>
+						</div>
+						<div className="space-y-8">
+							{recommended_projects.map((project, index) => (
+								<ListItem
+									key={index}
+									title={project.title}
+									description={project.description}
+								/>
+							))}
+						</div>
 					</div>
 				</div>
 			</main>
--- lib/create-github-client.ts
+++
@@ -1,5 +0,0 @@
-import { Octokit } from "@octokit/rest";
-
-export function createGitHubClient(accessToken: string) {
-	return new Octokit({ auth: accessToken });
-}
