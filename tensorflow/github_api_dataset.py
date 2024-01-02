3456ae383d9eae7be75c0f70632cbc270459d646
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-01-01 21:27:27 -0500

    improvements to dataset creation script

--- tensorflow/github_api_dataset.py
+++ tensorflow/github_api_dataset.py
@@ -1,7 +1,12 @@
-import datetime
-import requests
-import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+import pandas as pd
+import requests
+import datetime
+
+
+# Your GitHub Personal Access Token
+TOKEN = "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"
 
 
 """ RATE LIMITS """
@@ -15,23 +20,13 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 # No more than 100 concurrent request
 
 GITHUB_API = "https://api.github.com"
-# Replace with your GitHub token
-TOKEN = "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"  # doesnt work
 HEADERS = {"Authorization": f"token {TOKEN}"}
 
+# getting the rate limit status
+before_response = requests.get(f"{GITHUB_API}/rate_limit", headers=HEADERS)
 
-# uncomment to get current rate limit headers
-# response = requests.get(f"{GITHUB_API}/rate_limit", headers=HEADERS)
-# print(response.headers)
-
-# reset_time = int(response.headers['X-Ratelimit-Reset'])
-# reset_time = datetime.datetime.fromtimestamp(reset_time)
-
-# # Convert to 12-hour time format
-# reset_time = reset_time.strftime('%Y-%m-%d %I:%M:%S %p')
-
-# print(f"Rate limits will be reset at: {reset_time}")
-# quit()
+input(
+    f"\033[1m\033[31mWARNING: {before_response.headers['X-Ratelimit-Remaining']} requests remaining. Press Enter to continue...\033[0m\n")
 
 
 def fetch_user_details(username):
@@ -43,11 +38,16 @@ def fetch_user_details(username):
         return None
 
 
-def search_users(min_followers=25, max_followers=100):
+MIN_FOLLOWERS = 25
+MAX_FOLLOWERS = 1000
+NUM_USERS = 100
+
+
+def search_users(min_followers=MIN_FOLLOWERS, max_followers=MAX_FOLLOWERS):
     """ Search for users with followers in a specified range and located in the USA. """
     query = f"followers:{min_followers}..{max_followers}+location:USA"
     response = requests.get(
-        f"{GITHUB_API}/search/users?q={query}&per_page=100", headers=HEADERS)
+        f"{GITHUB_API}/search/users?q={query}&per_page={NUM_USERS}", headers=HEADERS)
     if response.status_code == 200:
         return response.json()['items']
     else:
@@ -106,7 +106,7 @@ def fetch_user_data(user):
         repos = get_user_repos(user['login'])
         if repos:
             experience_level = "Beginner" if len(
-                repos) < 10 else "Intermediate" if len(repos) < 30 else "Expert"
+                repos) < 25 else "Intermediate" if len(repos) < 50 else "Expert"
             languages, tech_keywords, projects = analyze_repos(repos)
             print(f"Data fetched for user: {user['login']}")
             return {
@@ -142,11 +142,28 @@ def create_dataset():
 
     # Save to CSV
     csv_filename = "github_users_dataset.csv"
-    dataset.to_csv(csv_filename, index=False)
-    print(f"Dataset saved to {csv_filename}")
+
+    # Check if file exists to avoid writing header multiple times
+    file_exists = os.path.isfile(csv_filename)
+
+    # Save to CSV
+    dataset.to_csv(csv_filename, mode='a', index=False, header=not file_exists)
+    print(f"New dataset saved to {csv_filename}")
 
     return dataset
 
 
 # Generate dataset
 dataset = create_dataset()
+
+# requests used up and requests remaining
+after_response = requests.get(f"{GITHUB_API}/rate_limit", headers=HEADERS)
+reset_time = int(before_response.headers['X-Ratelimit-Reset'])
+reset_time = datetime.datetime.fromtimestamp(reset_time)
+
+reset_time = reset_time.strftime('%Y-%m-%d %I:%M:%S %p')
+
+requests_used = int(after_response.headers['X-Ratelimit-Used'])
+
+print(
+    f"\033[1m\033[31mRate limits will be reset at: {reset_time} with {requests_used}/5000 requests used\033[0m\n")
